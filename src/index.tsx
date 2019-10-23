95baccf2bae4e5784236103ce4e9b2dcd4f4b926
Nebulis <nebounet@gmail.com>
2019-10-23 14:30:18 +0800

    feat: use legacy decentralized renderer

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
+import { LegacyFramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
 
-ReactDOM.render(<FramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
+ReactDOM.render(<LegacyFramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
