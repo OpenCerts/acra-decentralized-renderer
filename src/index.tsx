a2a789f4156b5fc67e37af7d26fa3980534507c9
Nebulis <nebounet@gmail.com>
2019-10-25 18:02:34 +0800

    fix: upgrade @govtechsg/decentralized-renderer-react-components to v1.1.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { LegacyFramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
+import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
 
-ReactDOM.render(<LegacyFramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
+ReactDOM.render(<FramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
