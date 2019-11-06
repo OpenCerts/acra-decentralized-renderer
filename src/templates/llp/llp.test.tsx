86a2e36762c99fd9ba00bc95a77b80e9ca89d7f3
Nebulis <nebounet@gmail.com>
2019-11-06 14:14:15 +0800

    fix: lint

--- src/templates/llp/llp.test.tsx
+++ src/templates/llp/llp.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Llp } from "./llp";
 import { llpCertificate } from "../sample";
 import { fireEvent, render } from "@testing-library/react";
